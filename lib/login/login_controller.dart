a2b7c8d78853d61defd5a6b01195e39fee8173bc
razer <santhoshcse619@gmail.com>
2024-01-24 20:29:28 +0530

    changes

--- lib/login/login_controller.dart
+++ lib/login/login_controller.dart
@@ -1,18 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 class LoginController {
-  // final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  // Future<User?> signInWithGitHub() async {
-  //   try {
-  //     // Trigger GitHub authentication
-  //     UserCredential authResult = await _auth.signInWithPopup(
-  //       GithubAuthProvider.credential(),
-  //     );
-
-  //     // Return the signed-in user
-  //     return authResult.user;
-  //   } catch (error) {
-  //     print('GitHub login failed: $error');
-  //     return null;
-  //   }
-  // }
+  late final FirebaseAuth _firebaseAuth;
+ LoginController(this._firebaseAuth);
+ Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+ Future<String?> signIn({required String token}) async {
+ try {
+ // Use the GitHub token to sign in with Firebase
+ await _firebaseAuth.signInWithCustomToken(token);
+ return "Signed in";
+ } on FirebaseAuthException catch (e) {
+ return e.message;
+ }
+ }
+ Future<void> signOut() async {
+ await _firebaseAuth.signOut();
+ }
 }
